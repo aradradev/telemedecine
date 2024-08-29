eef9165cec22b94c8ed9df47387c1a3b93f7bbfc
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-29 14:23:51 +0000

    create navlink and implementation

--- client/src/Header/Header.jsx
+++ client/src/Header/Header.jsx
@@ -1,4 +1,40 @@
+import { useEffect, useRef } from 'react'
+import logo from '../assets/images/vector/logoBlack.svg'
+import { NavLink, Link } from 'react-router-dom'
+
+const navLinks = [
+  {
+    path: '/home',
+    display: 'Home',
+  },
+  {
+    path: '/doctors',
+    display: 'Find  a Doctor',
+  },
+  {
+    path: '/services',
+    display: 'Services',
+  },
+  {
+    path: '/contact',
+    display: 'Contact',
+  },
+]
+
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <header className='header flex items-center'>
+      <div className='container'>
+        <div className='flex items-center justify-between'>
+          <div>
+            <img src={logo} alt='image logo' />
+          </div>
+          <div className='navigation'>
+            <ul className='menu flex items-center gap-[2.7rem]'></ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
 }
 export default Header
