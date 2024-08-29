56fd24949913453b826cf79c7336954dbcc29610
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-29 10:06:19 +0000

    add app js for basic server

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+require('dotenv').config()
+require('express-async-errors')
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+const start = async () => {
+  try {
+    app.listen(port, console.log(`Server is listening on port: ${port}`))
+  } catch (error) {
+    console.log(`Something went wrong - ${error}`)
+  }
+}
+
+start()
