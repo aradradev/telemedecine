5458c0d090b566270d926009e3b8f8eaa221addf
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-29 15:59:03 +0000

    add useRef to handle toggleMenu

--- client/src/Header/Header.jsx
+++ client/src/Header/Header.jsx
@@ -24,16 +24,36 @@ const navLinks = [
 ]
 
 const Header = () => {
+  const headerRef = useRef(null)
+  const menuRef = useRef(null)
+
+  const handleStickyHeader = () => {
+    window.addEventListener('scroll', () => {
+      if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        headerRef.current.classList.add('sticky__header')
+      } else {
+        headerRef.current.classList.remove('sticky__header')
+      }
+    })
+  }
+
+  useEffect(() => {
+    handleStickyHeader()
+    return () => window.removeEventListener('scroll', handleStickyHeader)
+  })
+
+  const toggleMenu = () => menuRef.current.classList.toggle('show__menu')
   return (
-    <header className='header flex items-center'>
+    <header className='header flex items-center' ref={headerRef}>
       <div className='container'>
         <div className='flex items-center justify-between'>
           {/* ===============logo=============== */}
           <div className='w-40'>
             <img src={logo} alt='image logo' />
           </div>
-          {/* ===============Navigation=============== */}
-          <div className='navigation'>
+
+          {/* ===============menu=============== */}
+          <div className='navigation' onClick={toggleMenu}>
             <ul className='menu flex items-center gap-[2.7rem]'>
               {navLinks.map((link, index) => (
                 <li key={index}>
@@ -64,7 +84,7 @@ const Header = () => {
                 Login
               </button>
             </Link>
-            <span className='md:hidden'>
+            <span className='md:hidden' onClick={toggleMenu}>
               <BiMenu className='w-6 h-6 cursor-pointer' />
             </span>
           </div>
