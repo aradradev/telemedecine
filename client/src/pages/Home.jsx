32faf261407812b02a7bd725099a11bbf2ea4e13
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-02 14:48:03 +0000

    add the icons

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -85,7 +85,24 @@ const Home = () => {
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]'>
             <div className='py-[30px] px-5'>
               <div className='flex items-center justify-center'>
-                <img src={icon1} alt='' />
+                <img src={icon1} alt='icon 1' />
+              </div>
+              <div className='mt-[30px]'>
+                <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>Find a Doctor</h2>
+                <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>
+                  Connect with our network of experienced healthcare professionals, ready to assist with your medical
+                  needs.
+                </p>
+                <Link
+                  to='/doctors'
+                  className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
+                  <BsArrowRight className='group-hover:text-white w-6 h-5' />
+                </Link>
+              </div>
+            </div>
+            <div className='py-[30px] px-5'>
+              <div className='flex items-center justify-center'>
+                <img src={icon1} alt='icon 2' />
               </div>
               <div className='mt-[30px]'>
                 <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>Find a Doctor</h2>
