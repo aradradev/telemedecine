57bfa7b3873d4bccce972aed0925d1e11b94ff18
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-02 13:39:52 +0000

    add hero content and setup hero counter

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -19,6 +19,31 @@ const Home = () => {
                   <button className='btn'>Request an Appointment</button>
                 </div>
                 {/** hero counter */}
+                <div className='mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-[30px]'>
+                  <div>
+                    <h2 className='text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor'>
+                      15+
+                    </h2>
+                    <span className='w-[100px] h-2 bg-orange-500 rounded-full block mt-[-14px]'></span>
+                    <p className='text_para'>Years of Experience</p>
+                  </div>
+
+                  <div>
+                    <h2 className='text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor'>
+                      500+
+                    </h2>
+                    <span className='w-[100px] h-2 bg-blue-400 rounded-full block mt-[-14px]'></span>
+                    <p className='text_para'>Customer Success Stories</p>
+                  </div>
+
+                  <div>
+                    <h2 className='text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor'>
+                      30+
+                    </h2>
+                    <span className='w-[100px] h-2 bg-red-400 rounded-full block mt-[-14px]'></span>
+                    <p className='text_para'>Clinic Partnerships</p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
