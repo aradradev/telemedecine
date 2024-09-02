3c8266e25b32e97e7c4e2d06cfecca03c53d5948
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-02 14:00:21 +0000

    hero section end

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
               <div>
                 <div className='lg:w-[570px]'>
                   <h1 className='text-[36px] leading-[46px] text-headingColor font-[600] md:text-[60px] md:leading-[70px]'>
-                    Your Health, Our Priority at <span className='text-cyan-600 font-[800]'>WeCareHealth</span>
+                    Your Health, Our Priority at <span className='text-primaryColor font-[800]'>WeCareHealth</span>
                   </h1>
                   <p className='text__para mt-4 text-[18px] leading-[28px] text-textColor font-[400] md:text-[22px] md:leading-[32px]'>
                     Your trusted partner in healthcare. Find doctors, book appointments, and access your medical history
@@ -63,6 +63,7 @@ const Home = () => {
           </div>
         </section>
       </>
+      {/** Hero section end*/}
     </>
   )
 }
