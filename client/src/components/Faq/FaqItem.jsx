07935440f9d4ad6473491609d2f92a700fe1859c
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-04 10:45:33 +0000

    add item props

--- client/src/components/Faq/FaqItem.jsx
+++ client/src/components/Faq/FaqItem.jsx
@@ -1,6 +1,11 @@
-const FaqItem = () => {
+import { useState } from 'react'
+const FaqItem = ({ item }) => {
   return (
-    <div>FaqItem</div>
+    <div className='p-3 lg:p-5 rounded-[12px] border border-solid border-[#d9dce2] mb-5 cursor-pointer'>
+      <div className='flex items-center justify-between gap-5'>
+        <h4 className='text-[16px] leading-7 lg:leading-8 text-headingColor'>{item.question}</h4>
+      </div>
+    </div>
   )
 }
 export default FaqItem
