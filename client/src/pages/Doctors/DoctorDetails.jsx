ba0c1ae0f1bcc8993560c9e5af67fb241e5fe7a3
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-04 21:57:52 +0000

    setTab for about & feedback

--- client/src/pages/Doctors/DoctorDetails.jsx
+++ client/src/pages/Doctors/DoctorDetails.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react'
 import imgDoctor from '../../assets/images/doctor-img02.png'
 import starIcon from '../../assets/images/Star.png'
 const DoctorDetails = () => {
+  const [tab, setTab] = useState('about')
   return (
     <section>
       <div className='max-w-[1170px] px-5 mx-auto'>
@@ -32,8 +34,14 @@ const DoctorDetails = () => {
             </div>
 
             <div className='mt-[50px] border-b border-solid border-[#0066ff34]'>
-              <button className={` py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold`}>About</button>
-              <button className={` py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold`}>
+              <button
+                onClick={() => setTab('about')}
+                className={` py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold`}>
+                About
+              </button>
+              <button
+                onClick={() => setTab('feedback')}
+                className={` py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold`}>
                 Feedback
               </button>
             </div>
