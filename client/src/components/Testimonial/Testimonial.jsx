5867dd608c2bf158f85519943ebf68800e3ec98b
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-04 12:46:44 +0000

    import all react library

--- client/src/components/Testimonial/Testimonial.jsx
+++ client/src/components/Testimonial/Testimonial.jsx
@@ -1,3 +1,10 @@
+import { Pagination } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import patientAvatar from '../../assets/images/patient-avatar.png'
+import { HiStar } from 'react-icons/hi'
+
 const Testimonial = () => {
   return <div>Testimonial</div>
 }
