b391455a575e29c82dde1505f68b6420d8bcff13
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-04 15:13:23 +0000

    import service data from data

--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -1,3 +1,7 @@
+import { services } from '../assets/data/services'
+import ServiceCard from '../components/Services/ServiceCard'
+import Testimonial from '../components/Testimonial/Testimonial'
+
 const Services = () => {
   return (
     <section>
