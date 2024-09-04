5689bd6ff272c6387ccfca64d86e6c883ae50a13
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-04 22:30:39 +0000

    add education details

--- client/src/pages/Doctors/DoctorAbout.jsx
+++ client/src/pages/Doctors/DoctorAbout.jsx
@@ -10,6 +10,19 @@ const DoctorAbout = () => {
           known for his precision and compassionate approach, ensuring the best outcomes for his patients.
         </p>
       </div>
+
+      <div className='mt-12'>
+        <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold'>Education</h3>
+        <ul className='pt-4 md:p-5'>
+          <li className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]'>
+            <div>
+              <span className='text-irisBlueColor text-[15px] leading-6 font-semibold '>04 September, 2010</span>
+              <p className='text-[16px] leading-6 font-medium text-textColor'>PHD in Surgeon</p>
+            </div>
+            <p className='text-[14px] leading-5 font-medium text-textColor'>New Appollo, New York.</p>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
