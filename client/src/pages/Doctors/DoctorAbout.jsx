3b63a1d15ceaf15be411cb281fd45dc14a887fbe
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-04 22:55:18 +0000

    add experience doctors

--- client/src/pages/Doctors/DoctorAbout.jsx
+++ client/src/pages/Doctors/DoctorAbout.jsx
@@ -40,7 +40,20 @@ const DoctorAbout = () => {
       <div className='mt-12'>
         <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold'>Experience</h3>
         <ul className='grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5'>
-          <li className='p-4 rounded bg-[#fff9ea]'></li>
+          <li className='p-4 rounded bg-[#fff9ea]'>
+            <span className='text-yellowColor text-[15px] leading-6 font-semibold'>
+              {formateDate('08-24-2015')} - {formateDate('10-01-2020')}
+            </span>
+            <p className='text-[16px] leading-6 font-medium text-textColor'>Sr. Surgeon</p>
+            <p className='text-[14px] leading-5 font-medium text-textColor'>New Appollo, New York.</p>
+          </li>
+          <li className='p-4 rounded bg-[#fff9ea]'>
+            <span className='text-yellowColor text-[15px] leading-6 font-semibold'>
+              {formateDate('08-24-2015')} - {formateDate('10-01-2020')}
+            </span>
+            <p className='text-[16px] leading-6 font-medium text-textColor'>Sr. Surgeon</p>
+            <p className='text-[14px] leading-5 font-medium text-textColor'>New Appollo, New York.</p>
+          </li>
         </ul>
       </div>
     </div>
