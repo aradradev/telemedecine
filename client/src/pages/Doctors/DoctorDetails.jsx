e5f2b2ab73ab20af7e8c23ed7bb982dda788fff4
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-04 21:48:21 +0000

    add doctors details

--- client/src/pages/Doctors/DoctorDetails.jsx
+++ client/src/pages/Doctors/DoctorDetails.jsx
@@ -15,10 +15,19 @@ const DoctorDetails = () => {
                   Surgeon
                 </span>
                 <h3 className='text-headingColor text-[22px] leading-9 mt-3 font-bold'>John Doe</h3>
-                <div className='flex items-center gap-[6px]'></div>
-                <span className='flex items-center gap-[6px] text-[14px] text-headingColor leading-5 lg:text-[16px] leading-7 font-semibold'>
-                  <img src={starIcon} alt='starIcon' /> 4.8
-                </span>
+                <div className='flex items-center gap-[6px]'>
+                  <span className='flex items-center gap-[6px] text-[14px] text-headingColor leading-5 lg:text-[16px] lg:leading-7 font-semibold'>
+                    <img src={starIcon} alt='starIcon' /> 4.8
+                  </span>
+                  <span className='text-[14px] text-textColor leading-5 lg:text-[16px] lg:leading-7 font-[400]'>
+                    (272)
+                  </span>
+                </div>
+                <p className='text__para text-[14px] leading-5 md:text-[15px] lg:max-w-[390px]'>
+                  Dr. John Doe is a highly skilled surgeon with over 10 years of experience in providing exceptional
+                  care. He is known for his precision and compassionate approach, ensuring the best outcomes for his
+                  patients.
+                </p>
               </div>
             </div>
           </div>
