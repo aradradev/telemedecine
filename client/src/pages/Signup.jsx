3dd6a2aebb416c0a0f7164ef09d65262868f6743
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-05 15:44:26 +0000

    handle file input change

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,30 @@
 import signupImg from '../assets/images/signup.gif'
 import avatar from '../assets/images/doctor-img02.png'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const Signup = () => {
+  const [selectedFile, setSelectedFile] = useState(null)
+  const [previewURL, setPreviewURL] = useState('')
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    gender: '',
+    role: 'patient',
+    photo: selectedFile,
+  })
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  const handleFileInputChange = async (e) => {
+    const file = e.target.files[0]
+    console.log(file)
+  }
+
   return (
     <section className='px-5 lg:px-0'>
       <div className='max-w-[1170px] mx-auto'>
@@ -24,7 +47,8 @@ const Signup = () => {
                   type='text'
                   placeholder='Full Name'
                   name='name'
-                  value=''
+                  value={formData.name}
+                  onChange={handleInputChange}
                   className='py-3 pr-4 w-full border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer'
                   required
                 />
@@ -32,7 +56,8 @@ const Signup = () => {
                   type='email'
                   placeholder='Enter your email'
                   name='email'
-                  value=''
+                  value={formData.email}
+                  onChange={handleInputChange}
                   className='py-3 pr-4 w-full border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer'
                   required
                 />
@@ -40,7 +65,8 @@ const Signup = () => {
                   type='password'
                   placeholder='Password'
                   name='password'
-                  value=''
+                  value={formData.password}
+                  onChange={handleInputChange}
                   className='py-3 pr-4 w-full border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer'
                   required
                 />
@@ -50,6 +76,8 @@ const Signup = () => {
                   Are you a:
                   <select
                     name='role'
+                    value={formData.role}
+                    onChange={handleInputChange}
                     className='text-textColor font-semibold text-[15px] leading-6 px-4 py-3 focus:outline-none '>
                     <option value='patient'>Patient</option>
                     <option value='doctor'>Doctor</option>
@@ -59,6 +87,8 @@ const Signup = () => {
                   Gender:
                   <select
                     name='gender'
+                    value={formData.gender}
+                    onChange={handleInputChange}
                     className='text-textColor font-semibold text-[15px] leading-6 px-4 py-3 focus:outline-none '>
                     <option value=''>Select</option>
                     <option value='male'>Male</option>
@@ -76,6 +106,7 @@ const Signup = () => {
                     type='file'
                     name='photo'
                     id='customFile'
+                    onChange={handleFileInputChange}
                     accept='jpeg, png, jpg'
                     className='absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer'
                   />
@@ -86,6 +117,19 @@ const Signup = () => {
                   </label>
                 </div>
               </div>
+              <div className='mt-5'>
+                <button
+                  className='bg-primaryColor text-white w-full py-3 px-4 text-[18px] rounded-lg leading-[30px]'
+                  type='submit'>
+                  Sign Up
+                </button>
+              </div>
+              <p className='mt-5 text-textColor text-center'>
+                Already have an account?
+                <Link to='/login' className='text-primaryColor font-medium ml-1'>
+                  Login
+                </Link>
+              </p>
             </form>
           </div>
         </div>
