04c4dfe088cc2b385cfda219f1c4849dab3ed6ac
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-05 16:10:16 +0000

    add email custom input

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -6,6 +6,14 @@ const Contact = () => {
         <p className='mb-8 lg:mb-16 font-light text-center text__para'>
           Got a technical issue? Want to send feature about beta feature? Let us know.
         </p>
+        <form action='#' className='space-y-8'>
+          <div>
+            <label htmlFor='email' className='form__label'>
+              Your Email
+            </label>
+            <input type='email' id='email' className='form__input mt-4' placeholder='example@gmail.com' />
+          </div>
+        </form>
       </div>
     </section>
   )
