a54cbcd60f5dd20568db7f2b760745170af6d594
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-05 13:44:54 +0000

    add time slots

---
+++ client/src/pages/Doctors/SidePanel.jsx
@@ -0,0 +1,28 @@
+const SidePanel = () => {
+  return (
+    <div className='shadow-panelShadow p-3 lg:p-5 rounded-md'>
+      <div className='flex items-center justify-between'>
+        <p className='text__para mt-0 font-semibold'>Ticket Price</p>
+        <span className='text-[16px] leading-7 lg:text-[22px] lg:leading-8 text-headingColor font-bold'>120</span>
+      </div>
+      <div className='mt-[30px]'>
+        <p className='text__para mt-0 font-semibold text-textColor'>Available Time Slots</p>
+        <ul className='mt-3'>
+          <li className='flex items-center justify-between mb-2'>
+            <p className='text-[15px] leading-6 font-semibold text-textColor'>Sunday</p>
+            <p className='text-[15px] leading-6 font-semibold text-textColor'>04:00 PM - 09-30 PM</p>
+          </li>
+          <li className='flex items-center justify-between mb-2'>
+            <p className='text-[15px] leading-6 font-semibold text-textColor'>Monday</p>
+            <p className='text-[15px] leading-6 font-semibold text-textColor'>04:00 PM - 09-30 PM</p>
+          </li>
+          <li className='flex items-center justify-between mb-2'>
+            <p className='text-[15px] leading-6 font-semibold text-textColor'>Tuesday</p>
+            <p className='text-[15px] leading-6 font-semibold text-textColor'>04:00 PM - 09-30 PM</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+export default SidePanel
