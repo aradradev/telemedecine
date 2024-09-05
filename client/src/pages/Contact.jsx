73b9ea28564ff7f17ff3db930714d36f7673b2d6
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-05 16:05:54 +0000

    add contact implementation

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,4 +1,13 @@
 const Contact = () => {
-  return <div>Contact</div>
+  return (
+    <section>
+      <div className='px-4 mx-auto max-w-screen-md'>
+        <h3 className='heading text-center'>Contact Us</h3>
+        <p className='mb-8 lg:mb-16 font-light text-center text__para'>
+          Got a technical issue? Want to send feature about beta feature? Let us know.
+        </p>
+      </div>
+    </section>
+  )
 }
 export default Contact
