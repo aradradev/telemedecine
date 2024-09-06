f5c5f22f71f93ea99b21714ac7e17e8bffc1b1e3
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-06 22:21:00 +0000

    add create jwt and token verification

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,5 +1,9 @@
 const createTokenUser = require('./createTokenUser')
+const { createJWT, isTokenValid, attachCookiesToResponse } = require('./jwt')
 
 module.exports = {
   createTokenUser,
+  createJWT,
+  isTokenValid,
+  attachCookiesToResponse,
 }
