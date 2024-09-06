3a0d853d1d09c0614110f42de6e3ea4534e7afd6
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-06 19:53:46 +0000

    add timestamps

--- server/models/Doctor.js
+++ server/models/Doctor.js
@@ -1,64 +1,67 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 
-const DoctorSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Please provide name'],
-    minlength: 3,
-    maxlength: 50,
-  },
-  email: {
-    type: String,
-    required: [true, 'Please provide email'],
-    validate: {
-      validator: validator.isEmail,
-      message: 'Please provide valid email',
+const DoctorSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please provide name'],
+      minlength: 3,
+      maxlength: 50,
     },
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, 'Please provide password'],
-    minlength: 6,
-  },
-  phone: { type: String },
-  photo: { type: String },
-  ticketPrice: { type: Number },
-  role: {
-    type: String,
-  },
-  specialization: {
-    type: String,
-    required: [true, 'Please provide specialization'],
-  },
-  qualifications: { type: Array },
-  experiences: { type: Array },
-  bio: { type: String, maxlength: 500 },
-  timeSlots: { type: Array },
-  reviews: [
-    {
+    email: {
+      type: String,
+      required: [true, 'Please provide email'],
+      validate: {
+        validator: validator.isEmail,
+        message: 'Please provide valid email',
+      },
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please provide password'],
+      minlength: 6,
+    },
+    phone: { type: String },
+    photo: { type: String },
+    ticketPrice: { type: Number },
+    role: {
+      type: String,
+    },
+    specialization: {
+      type: String,
+      required: [true, 'Please provide specialization'],
+    },
+    qualifications: { type: Array },
+    experiences: { type: Array },
+    bio: { type: String, maxlength: 500 },
+    timeSlots: { type: Array },
+    reviews: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: 'Review',
+      },
+    ],
+    averageRating: {
+      type: Number,
+      default: 0,
+    },
+    totalRating: {
+      type: Number,
+      default: 0,
+    },
+    isApproved: {
+      type: String,
+      enum: ['pending', 'approved', 'rejected'],
+      default: 'pending',
+    },
+    appointments: {
       type: mongoose.Types.ObjectId,
-      ref: 'Review',
+      ref: 'Appointment',
     },
-  ],
-  averageRating: {
-    type: Number,
-    default: 0,
   },
-  totalRating: {
-    type: Number,
-    default: 0,
-  },
-  isApproved: {
-    type: String,
-    enum: ['pending', 'approved', 'rejected'],
-    default: 'pending',
-  },
-  appointments: {
-    type: mongoose.Types.ObjectId,
-    ref: 'Appointment',
-  },
-})
+  { timestamps: true },
+)
 
 module.exports = mongoose.model('Doctor', DoctorSchema)
