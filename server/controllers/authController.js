8a8179f9116ca19222cf559533763aeadd500052
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-06 20:25:52 +0000

    add implementation of register route

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,15 @@
+const User = require('../models/User')
+const CustomError = require('../errors')
+const { StatusCodes } = require('http-status-codes')
+
 const register = async (req, res) => {
-  res.send('register route')
+  const { name, email, password } = req.body
+  const isEmailExist = User.findOne({ email })
+  if (isEmailExist) {
+    throw new CustomError.BadRequestError('Email already exists')
+  }
+  const user = User.create({ name, email, password })
+  res.status(StatusCodes.CREATED).json({ user })
 }
 
 const login = async (req, res) => {
