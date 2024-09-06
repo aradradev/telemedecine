d13dc8f2e6866e3428a6128549aaa9d07499201a
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-06 19:33:20 +0000

    complete ReviewSchema

--- server/models/Review.js
+++ server/models/Review.js
@@ -1,5 +1,25 @@
 const mongoose = require('mongoose')
 
-const ReviewSchema = new mongoose.Schema({})
+const ReviewSchema = new mongoose.Schema({
+  doctor: {
+    type: mongoose.Types.ObjectId,
+    ref: 'Doctor',
+  },
+  user: {
+    type: mongoose.Types.ObjectId,
+    ref: 'User',
+  },
+  reviewText: {
+    type: String,
+    required: [true, 'Please provide review'],
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 5,
+    default: 0,
+  },
+})
 
 module.exports = mongoose.model('Review', ReviewSchema)
