5d8d5a891372432f8d03382516b69b9ddda0c2e4
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-06 20:31:03 +0000

    remove some check in

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -4,10 +4,6 @@ const { StatusCodes } = require('http-status-codes')
 
 const register = async (req, res) => {
   const { name, email, password } = req.body
-  const isEmailExist = User.findOne({ email })
-  if (isEmailExist) {
-    throw new CustomError.BadRequestError('Email already exists')
-  }
   const user = User.create({ name, email, password })
   res.status(StatusCodes.CREATED).json({ user })
 }
