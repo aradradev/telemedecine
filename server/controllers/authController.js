ae9db90e4cb70e7accab5bc49ad1f1b989b0b31f
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-06 21:16:30 +0000

    check if email already exist

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -4,6 +4,12 @@ const { StatusCodes } = require('http-status-codes')
 
 const register = async (req, res) => {
   const { name, email, password } = req.body
+
+  const isEmailExist = await User.findOne({ email })
+  if (isEmailExist) {
+    throw new CustomError.BadRequestError('Email already exist')
+  }
+
   const user = await User.create({ name, email, password })
   const tokenUser = { name: user.name, userId: user._id, role: user.role }
   res.status(StatusCodes.CREATED).json({ user: tokenUser })
