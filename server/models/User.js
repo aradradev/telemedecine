3b54e5da6358328d08ead10140aa0e7d55f8b9e9
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-06 18:43:03 +0000

    add name:{type:String} & email:{type:String}

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose')
 
-const UserSchema = new mongoose.Schema({})
+const UserSchema = new mongoose.Schema({
+ name:{
+  type: String,
+  required: [true, 'Please provide name'],
+  minlength:3,
+  maxlength:50
+ },
+ email:{
+  type: String,
+  required:[true, 'Please provide email'],
+  validate: {
+   validator: validator.isEmail,
+   message: 'Please provide valid email'
+  }
+ }
+})
 
 module.exports = mongoose.model('User', UserSchema)
