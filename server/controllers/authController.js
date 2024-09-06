88147561818fb2362171bb746dda63439f88b455
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-06 21:41:25 +0000

    add check if user already exist or not

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,22 +1,35 @@
 const User = require('../models/User')
+const Doctor = require('../models/Doctor')
 const CustomError = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const { createTokenUser } = require('../utils')
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body
+  const { name, email, password, role, photo, gender } = req.body
 
-  const isEmailExist = await User.findOne({ email })
-  if (isEmailExist) {
-    throw new CustomError.BadRequestError('Email already exist')
+  // const isEmailExist = await User.findOne({ email })
+  // if (isEmailExist) {
+  //   throw new CustomError.BadRequestError('Email already exist')
+  // }
+
+  // const isFirstAccount = (await User.countDocuments({})) === 0
+  // const role = isFirstAccount ? 'admin' : 'patient'
+
+  // const user = await User.create({ name, email, password, role })
+  // const tokenUser = createTokenUser(user)
+  // res.status(StatusCodes.CREATED).json({ user: tokenUser })
+
+  let user = null
+
+  if (role === 'patient') {
+    user = User.findOne({ email })
+  } else if (role === 'doctor') {
+    user = Doctor.findOne({ email })
   }
 
-  const isFirstAccount = (await User.countDocuments({})) === 0
-  const role = isFirstAccount ? 'admin' : 'patient'
-
-  const user = await User.create({ name, email, password, role })
-  const tokenUser = createTokenUser(user)
-  res.status(StatusCodes.CREATED).json({ user: tokenUser })
+  if (user) {
+    throw new CustomError.BadRequestError('User already exist')
+  }
 }
 
 const login = async (req, res) => {
