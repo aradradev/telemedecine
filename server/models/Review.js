78010fb4a9ddb610158383b7c9b5c014de8a2e32
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-06 19:33:57 +0000

    adding timestamps to true;

--- server/models/Review.js
+++ server/models/Review.js
@@ -1,25 +1,28 @@
 const mongoose = require('mongoose')
 
-const ReviewSchema = new mongoose.Schema({
-  doctor: {
-    type: mongoose.Types.ObjectId,
-    ref: 'Doctor',
+const ReviewSchema = new mongoose.Schema(
+  {
+    doctor: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Doctor',
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+    },
+    reviewText: {
+      type: String,
+      required: [true, 'Please provide review'],
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 5,
+      default: 0,
+    },
   },
-  user: {
-    type: mongoose.Types.ObjectId,
-    ref: 'User',
-  },
-  reviewText: {
-    type: String,
-    required: [true, 'Please provide review'],
-  },
-  rating: {
-    type: Number,
-    required: true,
-    min: 0,
-    max: 5,
-    default: 0,
-  },
-})
+  { timestamps: true },
+)
 
 module.exports = mongoose.model('Review', ReviewSchema)
