af7c89abba85d03c447362a0a0f501f02f320d4d
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-06 19:12:43 +0000

    add DoctorSchema name email password specialization

--- server/models/Doctor.js
+++ server/models/Doctor.js
@@ -1,5 +1,45 @@
 const mongoose = require('mongoose')
 
-const DoctorSchema = new mongoose.Schema({})
+const DoctorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide name'],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide email'],
+    validate: {
+      validator: validator.isEmail,
+      message: 'Please provide valid email',
+    },
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minlength: 6,
+  },
+  phone: { type: Number },
+  photo: { type: String },
+  ticketPrice: { type: Number },
+  role: {
+    type: String,
+  },
+  specialization: {
+    type: String,
+  },
+  qualifications: { type: Array },
+  experiences: { type: Array },
+  bio: { type: String, maxlength: 50 },
+  timeSlots: { type: Array },
+  reviews: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: 'Review',
+    },
+  ],
+})
 
 module.exports = mongoose.model('Doctor', DoctorSchema)
