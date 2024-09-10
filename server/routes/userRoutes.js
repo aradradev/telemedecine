5eca42b64e9132d192ae3774714ddd412ecf6d51
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-10 10:19:19 +0000

    add user route

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const { getAllUsers, showCurrentUser, updateUser, getSingleUser } = require('../controllers/userController')
+
+const router = express.Router()
+
+router.route('/').get(getAllUsers)
+
+router.route('/showMe').get(showCurrentUser)
+router.route('/updateUser').patch(updateUser)
+router.route('/updateUserPassword').patch(updateUserPassword)
+
+// single user
+router.route('/:id').get(getSingleUser)
