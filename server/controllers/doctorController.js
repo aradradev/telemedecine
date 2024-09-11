14cc031565b5a7c897b711531bb34fa18b94cc8f
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-11 08:24:58 +0000

    implemnt basic setup for doctors

---
+++ server/controllers/doctorController.js
@@ -0,0 +1,19 @@
+const getAllDoctors = async (req, res) => {
+  res.send('get all doctors')
+}
+const getSingleDoctor = async (req, res) => {
+  res.send('get single doctor')
+}
+const showCurrentDoctor = async (req, res) => {
+  res.send('show current doctor')
+}
+const updateDoctor = async (req, res) => {
+  res.send('update doctor')
+}
+
+module.exports = {
+  getAllDoctors,
+  getSingleDoctor,
+  showCurrentDoctor,
+  updateDoctor,
+}
