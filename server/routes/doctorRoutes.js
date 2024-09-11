e2396b1f2ee89d6f5262e757074d2201d311c827
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-11 09:29:29 +0000

    add authorization

--- server/routes/doctorRoutes.js
+++ server/routes/doctorRoutes.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const { getAllDoctors, updateDoctor, deleteDoctor, getSingleDoctor } = require('../controllers/doctorController')
 
-const { authenticateUser } = require('../middleware/authentication')
+const { authenticateUser, authorizedPermissions } = require('../middleware/authentication')
 
 const router = express.Router()
 
 router.route('/').get(authenticateUser, getAllDoctors)
 
-router.route('/:id').get(getSingleDoctor).patch(updateDoctor).delete(deleteDoctor)
+router
+  .route('/:id')
+  .get(authenticateUser, getSingleDoctor)
+  .patch(authenticateUser, authorizedPermissions('admin', 'doctor'), updateDoctor)
+  .delete(authenticateUser, authorizedPermissions('admin', 'doctor'), deleteDoctor)
 
 module.exports = router
