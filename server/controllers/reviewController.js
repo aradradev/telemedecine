7eefbf4abbb2f8dad258c152d1e737469db50492
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-11 11:19:42 +0000

    implement delete review

--- server/controllers/reviewController.js
+++ server/controllers/reviewController.js
@@ -40,7 +40,15 @@ const updateReview = async (req, res) => {
   res.status(StatusCodes.OK).json({ review })
 }
 const deleteReview = async (req, res) => {
-  res.send('delete review')
+  const { id: reviewId } = req.params
+  const review = await Review.findOne({ _id: reviewId })
+  if (!review) {
+    throw new CustomError.NotFoundError(`No review with id: ${reviewId}`)
+  }
+
+  checkPermissions(req.user, review.user)
+  await review.deleteOne()
+  res.status(StatusCodes.OK).json({ message: 'Review deleted successfully' })
 }
 
 module.exports = {
