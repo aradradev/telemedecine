a5c2b479860aa686ea469d6252a3eed5b5db8043
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-12 18:41:29 +0000

    merge params

--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { getAllReviews, createReview, updateReview, deleteReview } = require('../controllers/reviewController')
 const { authenticateUser, authorizedPermissions } = require('../middleware/authentication')
-const router = express.Router()
+const router = express.Router({ mergeParams: true })
 
 router.route('/').get(getAllReviews).post(authenticateUser, authorizedPermissions('patient'), createReview)
 router.route('/:id').patch(authenticateUser, updateReview).delete(authenticateUser, deleteReview)
