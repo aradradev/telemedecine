3c46894095b5e9d620825043b92c42caca993764
Abdoul <abdoulramanediallo44@gmail.com>
2024-09-19 11:35:50 +0000

    add mybooking file implementaion

--- client/src/Dashboard/user-account/MyAccount.jsx
+++ client/src/Dashboard/user-account/MyAccount.jsx
@@ -14,8 +14,8 @@ const MyAccount = () => {
   return (
     <section>
       <div className='max-w-[1170px] px-5 mx-auto'>
-        {loading && <Loading />}
-        {error && <Error errMessage={error} />}
+        {loading && !error && <Loading />}
+        {error && !loading && <Error errMessage={error} />}
         {!loading && !error && (
           <div className='grid md:grid-cols-3 gap-10'>
             <div className='pb-[50px] px-[30px] rounded-md'>
--- client/src/Dashboard/user-account/MyBookings.jsx
+++ client/src/Dashboard/user-account/MyBookings.jsx
@@ -1,4 +1,28 @@
+import Error from '../../components/Error/Error'
+import Loading from '../../components/Loader/Loading'
+import { BASE_URL } from '../../config'
+import useFetchData from '../../hooks/useFetchData'
+import { DoctorCard } from '../../components/Doctors/DoctorCard'
+
 const MyBookings = () => {
-  return <div>MyBookings</div>
+  const { data: appointments, loading, error } = useFetchData(`${BASE_URL}/appointments/my-appointments`)
+  return (
+    <div>
+      {loading && !error && <Loading />}
+      {error && !loading && <Error errMessage={error} />}
+      {!loading && !error && (
+        <div className='grid grid-cols-1 lg:grid-cols-2 gap-5'>
+          {appointments.map((doctor) => (
+            <DoctorCard key={doctor._id} doctor={doctor} />
+          ))}
+        </div>
+      )}
+      {!loading && !error && appointments.length === 0 && (
+        <h1 className='mt-5 text-center leading-7  text-[20px] font-semibold text-primaryColor'>
+          You did not book any doctor yet
+        </h1>
+      )}
+    </div>
+  )
 }
 export default MyBookings
