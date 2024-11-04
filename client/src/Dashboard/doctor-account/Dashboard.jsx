89ff419f64a694d45032c48e0beb4948f2724dce
Abdoul <abdoulramanediallo44@gmail.com>
2024-11-04 16:05:41 +0000

    add doctor about from real data api

--- client/src/Dashboard/doctor-account/Dashboard.jsx
+++ client/src/Dashboard/doctor-account/Dashboard.jsx
@@ -4,12 +4,13 @@ import useGetProfile from '../../hooks/useFetchData'
 import { BASE_URL } from '../../config'
 import Tabs from './Tabs'
 import { useState } from 'react'
+import StarIcon from '../../assets/images/Star.png'
+import DoctorAbout from '../../pages/Doctors/DoctorAbout'
 
 const Dashboard = () => {
   const [tab, setTab] = useState('overview')
   const { data, loading, error } = useGetProfile(`${BASE_URL}/doctors/profile/me`)
-  console.log('profile Doctor Data: ', data.doctor.isApproved)
-  // console.log('profile data: ', data.doctor.isApproved)
+  console.log('profile data: ', data)
 
   return (
     <section>
@@ -38,12 +39,47 @@ const Dashboard = () => {
                       clipRule='evenodd'></path>
                   </svg>
                   <span className='sr-only'>Info</span>
-                  <div>
+                  <div className='ml-3 text-sm font-medium'>
                     To get approval, please complete your profile. We&apos;ll review manually and approve within
                     3-Business-Day
                   </div>
                 </div>
               )}
+              <div className='mt-8'>
+                {tab === 'overview' && (
+                  <div>
+                    <div className='flex items-center gap-4 mb-10'>
+                      <figure className='max-w-[200px] max-h-[200px]'>
+                        <img src={data?.doctor?.photo} alt='doctor image' className='w-full' />
+                      </figure>
+                      <div>
+                        <span className='bg-[#ccf0f3] text-irisBlueColor py-1 px-4 lg:py-2 lg:px-6 rounded text-[12px] leading-4 lg:text-[16px] lg:leading-6 font-semibold'>
+                          {data?.doctor?.specialization}
+                        </span>
+                        <h3 className='text-[22px] leading-9 font-bold text-headingColor mt-3'>{data?.doctor?.name}</h3>
+                        <div className='flex items-center gap-[6px]'>
+                          <span className='flex items-center gap-[6px] text-headingColor text-[14px] leading-5 lg:text-[16px] lg:leading-6 font-semibold'>
+                            <img src={StarIcon} alt='star image' />
+                            4.5
+                          </span>
+                          <span className=' text-textColor text-[14px] leading-5 lg:text-[16px] lg:leading-6 font-semibold'>
+                            (233)
+                          </span>
+                        </div>
+                        <p className='text__para font-[15px] lg:max-w-[390px] leading-6'>Doctor Bio</p>
+                      </div>
+                    </div>
+                    <DoctorAbout
+                      name={data?.doctor?.name}
+                      bio={data?.doctor?.bio}
+                      qualifications={data?.doctor?.qualifications}
+                      experiences={data?.doctor?.experiences}
+                    />
+                  </div>
+                )}
+                {tab === 'appointments' && <div>Appointments</div>}
+                {tab === 'settings' && <div>Profile Settings</div>}
+              </div>
             </div>
           </div>
         )}
--- client/src/pages/Doctors/DoctorAbout.jsx
+++ client/src/pages/Doctors/DoctorAbout.jsx
@@ -1,11 +1,12 @@
+/* eslint-disable react/prop-types */
 import { formateDate } from '../../utils/formateDate'
 
-const DoctorAbout = () => {
+const DoctorAbout = ({ name, bio, qualifications, experiences }) => {
   return (
     <div>
       <div>
         <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2'>
-          About <span className='text-irisBlueColor font-bold text-[24px] leading-9'>John Doe</span>
+          About <span className='text-irisBlueColor font-bold text-[24px] leading-9'>{name}</span>
         </h3>
         <p className='text__para'>
           Dr. John Doe is a highly skilled surgeon with over 10 years of experience in providing exceptional care. He is
