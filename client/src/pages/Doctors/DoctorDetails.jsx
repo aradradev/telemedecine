fb5c97bc4e88bf9aa0870dd87b4ffe865d6b928d
Abdoul <abdoulramanediallo44@gmail.com>
2024-11-27 10:14:34 +0000

    add doctor details

--- client/src/pages/Doctors/DoctorDetails.jsx
+++ client/src/pages/Doctors/DoctorDetails.jsx
@@ -14,22 +14,22 @@ import { useParams } from 'react-router-dom'
 const DoctorDetails = () => {
   const { id } = useParams()
   const { data: doctor, loading, error } = useFetchData(`${BASE_URL}/doctors/${id}`)
+  const {
+    name,
+    specialization,
+    averageRating,
+    totalRating,
+    photo,
+    totalPatients = 233,
+    experiences,
+    qualifications,
+    timeSlots,
+    reviews,
+    bio,
+    about,
+    ticketPrice,
+  } = doctor
   console.log(doctor)
-  // const {
-  //   name,
-  //   specialization,
-  //   averageRating,
-  //   totalRating,
-  //   photo,
-  //   totalPatients = 233,
-  //   experiences,
-  //   qualifications,
-  //   timeSlots,
-  //   reviews,
-  //   bio,
-  //   about,
-  //   ticketPrice,
-  // } = doctor
   const [tab, setTab] = useState('about')
   return (
     <section>
@@ -41,25 +41,23 @@ const DoctorDetails = () => {
             <div className='md:col-span-2'>
               <div className='flex items-center gap-5'>
                 <figure className='max-w-[200px] max-h-[200px]'>
-                  <img src={imgDoctor} alt='imgDoctor' className='w-full' />
+                  <img src={doctor?.doctor?.photo} alt='imgDoctor' className='w-full rounded-lg' />
                 </figure>
                 <div>
                   <span className='bg-[#ccf0f3] text-irisBlueColor py-1 px-6 lg:py-2 lg:px-6 text-[12px] leading-4 lg:text-[16px] lg:leading-7 font-semibold rounded'>
-                    Surgeon
+                    {doctor?.doctor?.specialization}
                   </span>
-                  <h3 className='text-headingColor text-[22px] leading-9 mt-3 font-bold'>John Doe</h3>
+                  <h3 className='text-headingColor text-[22px] leading-9 mt-3 font-bold'>{doctor?.doctor?.name}</h3>
                   <div className='flex items-center gap-[6px]'>
                     <span className='flex items-center gap-[6px] text-[14px] text-headingColor leading-5 lg:text-[16px] lg:leading-7 font-semibold'>
-                      <img src={starIcon} alt='starIcon' /> 4.8
+                      <img src={starIcon} alt='starIcon' /> {doctor?.doctor?.reviews || '4.5'}
                     </span>
                     <span className='text-[14px] text-textColor leading-5 lg:text-[16px] lg:leading-7 font-[400]'>
                       (272)
                     </span>
                   </div>
                   <p className='text__para text-[14px] leading-6 md:text-[15px] lg:max-w-[390px]'>
-                    Dr. John Doe is a highly skilled surgeon with over 10 years of experience in providing exceptional
-                    care. He is known for his precision and compassionate approach, ensuring the best outcomes for his
-                    patients.
+                    {doctor?.doctor?.about}
                   </p>
                 </div>
               </div>
